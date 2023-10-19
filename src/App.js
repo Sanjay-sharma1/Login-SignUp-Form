2390e7a9bcdbc3cab17c639329747287eace5774
Sanjay-sharma1 <sanjaysharma6189@gmail.com>
2023-10-19 18:29:51 +0530

    Login-Form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Login from './components/loginSignup/login';
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/loginSignup/login.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './login.css';
+
+const Login = () => {
+
+  const [action, setAction] = useState("Sign Up");
+
+  return (
+    <div className='container'>
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action==="Login" ? <div></div> : <div className="input">
+          <i className="fi fi-rr-user absolute-center"></i>
+          <input type="text" placeholder='Name'/>
+        </div>}
+
+        <div className="input">
+          <i className="fi fi-rr-envelope absolute-center"></i>
+          <input type="email"  placeholder='Email Id'/>
+        </div>
+
+        <div className="input">
+          <i className="fi fi-rr-lock absolute-center"></i>
+          <input type="password"  placeholder='Password'/>
+        </div>
+      </div>
+      
+      {action==="Sign Up" ? <div></div> : <div className="forgot-password">Forgot Password ? <span>Click Here</span></div>}
+      
+      <div className="submit-container">
+        <div className={action==="Login" ? "submit gray" : "submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action==="Sign Up" ? "submit gray" : "submit"} onClick={()=>{setAction("Login")}}>Login</div>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
